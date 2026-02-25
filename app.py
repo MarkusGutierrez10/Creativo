5f934b2e54a479e0121bb2493bed8224e2b1c66c
Markus Gutierrez <markusgutierrez10@gmail.com>
2026-02-24 21:50:15 -0500

    creacion de contacto

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("view/public/login.html")
+    return render_template("index.html")
+
+@app.route("/<url>")
+def redirigir(url):
+    try:
+        return render_template(f"view/{url}.html")
+    except:
+        abort(404)
 
 if __name__ == "__main__":
     app.run(debug=True)
